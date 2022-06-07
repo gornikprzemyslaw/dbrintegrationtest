c8632a9317c75221b4dd33f74d3b1f1218402a77
gornikprzemyslaw <gornikprzemyslaw>
2022-06-07 11:00:15 +0000

    as

--- github_integration.py
+++ github_integration.py
@@ -1,4 +1,4 @@
 # Databricks notebook source
 param = dbutils.widgets.get("param")
 
-print(f"Example of integration using notebook with additional info9 - {param}")
+print(f"Example of integration using notebook with additional info19 - {param}")
